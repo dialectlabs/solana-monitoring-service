9f33ddad37bf74c2330e328568f4020b32656caa
Aleksei <Apetrov@massive.co>
2022-06-08 23:13:40 +0300

    added test script to play with solana cli

---
+++ src/test.ts
@@ -0,0 +1,23 @@
+import * as web3 from '@solana/web3.js';
+var semver = require('semver');
+var exec = require('child_process').exec;
+
+const getSomething = async () => {
+  let connection = new web3.Connection(web3.clusterApiUrl('mainnet-beta'));
+  console.log(web3);
+  console.log(await connection.getVersion());
+  // @ts-ignore
+  exec('solana feature status', (error, stdout, stderr) => {
+    const versions = stdout.match(/\d+\.\d{1,2}\.\d{1,2}/g);
+    console.log(versions);
+    var max = versions.sort(semver.rcompare);
+    console.log(max);
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+};
+
+getSomething();
