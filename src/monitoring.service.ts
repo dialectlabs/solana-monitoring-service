aa9d9e11cd4fad1b438c88d1ada8e97e484dfdd5
Aleksei <Apetrov@massive.co>
2022-06-09 23:01:42 +0300

    update regexp, added logic to monitor

--- src/monitoring.service.ts
+++ src/monitoring.service.ts
@@ -1,6 +1,23 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { Monitors, Pipelines, SubscriberState } from '@dialectlabs/monitor';
+import {
+  Monitors,
+  Pipelines,
+  ResourceId,
+  SourceData,
+} from '@dialectlabs/monitor';
 import { DialectConnection } from './dialect-connection';
+import { Duration } from 'luxon';
+import { fetchCurrentVersion } from './version-service/version-service';
+import { PublicKey } from '@solana/web3.js';
+
+interface Version {
+  version: string;
+  subscribers: PublicKey[];
+}
+
+const groupingPK = new PublicKey(
+  '54MVx92iDeTmjcPxYkVUKSVE5iyd7KiPBAH4232A6tDP',
+);
 
 @Injectable()
 export class MonitoringService implements OnModuleInit, OnModuleDestroy {
@@ -11,19 +28,40 @@ export class MonitoringService implements OnModuleInit, OnModuleDestroy {
       monitorKeypair: this.dialectConnection.getKeypair(),
       dialectProgram: this.dialectConnection.getProgram(),
     })
-    .subscriberEvents()
-    .transform<SubscriberState, SubscriberState>({
-      keys: ['state'],
-      pipelines: [Pipelines.notifyNewSubscribers()],
-    })
-    .notify()
-    .dialectThread(({ value }) => ({
-      message: `Say hello to the future of web3 messaging.`,
-    }))
-    .and()
-    .dispatch('unicast')
-    .build();
+      .defineDataSource<Version>()
+      .poll(
+        async (subscribers) => this.getLatestVerion(subscribers),
+        Duration.fromObject({ second: 10 }),
+      )
+      .transform<string, string>({
+        keys: ['version'],
+        pipelines: Pipelines.createNew(),
+      })
+      .notify()
+      .dialectThread(
+        ({ value }) => ({
+          message: `Say hello to the future of web3 messaging.`,
+        }),
+        { dispatch: 'multicast', to: ({ origin }) => origin.subscribers },
+      )
+      .and()
+      .build();
     monitor.start();
+  }
+
+  async getLatestVerion(
+    subscribers: ResourceId[],
+  ): Promise<SourceData<Version>[]> {
+    const version = (await fetchCurrentVersion()) || '';
+    return [
+      {
+        groupingKey: groupingPK.toBase58(),
+        data: {
+          version: version,
+          subscribers: subscribers,
+        },
+      },
+    ];
   }
 
   async onModuleDestroy() {
