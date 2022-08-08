586f9f174eb7b85c66bca0aa24f9f285e85dfe74
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-08-08 12:29:15 +0400

    Use correct rpc url from env vars

--- src/version-service/version-service.ts
+++ src/version-service/version-service.ts
@@ -1,4 +1,3 @@
-import { RPC_URL } from '../dialect-connection';
 import { FeatureRelease } from '../monitoring.service';
 import { parseActiveHashes } from '../utils/parsing-utils';
 import { exec as process_exec } from 'child_process';
@@ -7,7 +6,11 @@ import { promisify } from 'util';
 const exec = promisify(process_exec);
 
 export async function fetchFeatureSet(): Promise<FeatureRelease[]> {
-  const { stdout } = await exec(`solana --url ${RPC_URL} feature status`);
+  const { stdout } = await exec(
+    `solana --url ${
+      process.env.SOLANA_RPC_URL ?? process.env.DIALECT_SDK_SOLANA_RPC_URL
+    } feature status`,
+  );
   if (process.env.TEST) {
     const hashes = parseActiveHashes(stdout);
     const spliced = hashes.splice(
