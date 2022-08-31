32a97cce5c985f9e556b0c292397a5b820e11dd9
Aleksei <Apetrov@massive.co>
2022-08-31 16:10:57 +0300

    fix: change source of truth for features update to git file

--- src/utils/parsing-utils.ts
+++ src/utils/parsing-utils.ts
@@ -1,46 +1,29 @@
+import axios from 'axios';
 import { FeatureRelease } from 'src/monitoring.service';
 
-export function pruneOutput(output: string): string[] {
-  const columnDelimiterRegExp = /.+\|.+\|.+/gm;
-  return [...output.matchAll(columnDelimiterRegExp)]
-    .filter((e) => e.length == 1)
-    .map((e) => e[0]!.trim());
-}
+export async function parseFeatureSDKFile(): Promise<FeatureRelease[]> {
+  try {
+    const req = await axios.get(
+      'https://raw.githubusercontent.com/solana-labs/solana/master/sdk/src/feature_set.rs',
+    );
 
-export function pruneActiveOutput(content: string): string[] {
-  const prunedOutput = pruneOutput(content);
-  return prunedOutput.filter((e) => featureSetIsActive(e));
-}
+    if (!req.data && req.data.length === 0) {
+      return [];
+    }
+    const regx = /\(.*::id\(\),[ ?]".*\)/gm;
+    const matches = req.data.match(regx);
 
-export function parseFeatureSetHashFromActiveLine(
-  line: string,
-): FeatureRelease | undefined {
-  const columns = line.split('|');
-  if (columns.length < 3) {
-    return undefined;
+    const finalMap = matches.map((it: any) => {
+      const descRegex = /".*"/gm;
+
+      return {
+        featureHash: it.split(',')[0].replace('(', '').replace('::id()', ''),
+        description: it.match(descRegex)[0].replace('"', '').replace('"', ''),
+      };
+    });
+
+    return finalMap;
+  } catch (e) {
+    return [];
   }
-  return {
-    featureHash: columns[0]!.trim(),
-    description: columns[2]!.trim(),
-  };
-}
-
-export function parseActiveHashes(content: string): FeatureRelease[] {
-  const activeLines = pruneActiveOutput(content);
-  const featureReleases: Map<string, FeatureRelease> = new Map(
-    activeLines
-      .map((e) => parseFeatureSetHashFromActiveLine(e))
-      .filter((e) => e)
-      .map((e) => [e!.featureHash, e!]),
-  );
-  return [...featureReleases.values()];
-}
-
-export function featureSetIsActive(featureSet: string): boolean {
-  const activityLine = featureSet.split('|')[1];
-  return (
-    activityLine != undefined &&
-    !activityLine.includes('inactive') &&
-    activityLine.includes('active')
-  );
 }
--- src/version-service/version-service.spec.ts
+++
@@ -1,9 +0,0 @@
-import { fetchFeatureSet } from './version-service';
-jest.setTimeout(10000000);
-
-describe('fetch tests', function () {
-  it('fetch success', async function () {
-    const version = await fetchFeatureSet();
-    console.log(version);
-  });
-});
