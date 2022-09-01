b631680b3a92c69ad7a1ecfce410c7897a2c3d98
Aleksei <Apetrov@massive.co>
2022-09-01 16:25:59 +0300

    remove try/catch

--- src/utils/parsing-utils.ts
+++ src/utils/parsing-utils.ts
@@ -2,29 +2,25 @@ import axios from 'axios';
 import { FeatureRelease } from 'src/monitoring.service';
 
 export async function parseFeatureSDKFile(): Promise<FeatureRelease[]> {
-  try {
-    const req = await axios.get(
-      'https://raw.githubusercontent.com/solana-labs/solana/master/sdk/src/feature_set.rs',
-    );
+  const req = await axios.get(
+    'https://raw.githubusercontent.com/solana-labs/solana/master/sdk/src/feature_set.rs1',
+  );
 
-    if (!req.data && req.data.length === 0) {
-      throw 'No data found in file';
-    }
-
-    const regx = /\(.*::id\(\),[ ?]".*\)/gm;
-    const matches = req.data.match(regx);
-
-    const finalMap = matches.map((it: any) => {
-      const descRegex = /".*"/gm;
-
-      return {
-        featureHash: it.split(',')[0].replace('(', '').replace('::id()', ''),
-        description: it.match(descRegex)[0].replace('"', '').replace('"', ''),
-      };
-    });
-
-    return finalMap;
-  } catch (e) {
-    throw e;
+  if (!req.data && req.data.length === 0) {
+    throw 'No data found in file';
   }
+
+  const regx = /\(.*::id\(\),[ ?]".*\)/gm;
+  const matches = req.data.match(regx);
+
+  const finalMap = matches.map((it: any) => {
+    const descRegex = /".*"/gm;
+
+    return {
+      featureHash: it.split(',')[0].replace('(', '').replace('::id()', ''),
+      description: it.match(descRegex)[0].replace('"', '').replace('"', ''),
+    };
+  });
+
+  return finalMap;
 }
